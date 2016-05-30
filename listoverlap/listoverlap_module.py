b9568c49b2a8520251a7956db514b9273bb6422f
Miklos Csepella <csepella.miki@gmail.com>
2016-05-30 14:34:48 +0200

    made listoverlap working

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -1,8 +1,18 @@
 def listoverlap(list1, list2):
-    return
+    c = []
+    for i in list1:
+        for j in list2:
+            if i == j:
+                if j not in c:
+                    c.append(j)
+    return c
 
 
 def main():
+    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    c = listoverlap(a, b) # other c than in listoverlap
+    print(c)
     return
 
 
