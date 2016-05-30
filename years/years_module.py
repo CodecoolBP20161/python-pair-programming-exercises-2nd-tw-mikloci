4fb9e3b1657d18b0202d7e3754eabd849ca57be2
Miklos Csepella <csepella.miki@gmail.com>
2016-05-30 14:05:19 +0200

    made year working

--- years/years_module.py
+++ years/years_module.py
@@ -1,12 +1,18 @@
-import datetime
+from datetime import date
 
 
 def years(age):
-    return
+    this_year = date.today().year
+    hundred_age = 100 - age + this_year
+    return hundred_age
 
 
 def main():
-    return
+    name = input('What is your name?: ')
+    age = int(input("How old are you?: "))
+    age = years(age)
+    print("Dear %s you are gonna be 100 years old in %d." % (name, age))
+    return age
 
 
 if __name__ == '__main__':
