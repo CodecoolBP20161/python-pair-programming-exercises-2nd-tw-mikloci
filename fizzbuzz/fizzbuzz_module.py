8d2774c384ec0d7bd4d51e8acda6b7174947ca5c
Miklos Csepella <csepella.miki@gmail.com>
2016-05-30 13:34:20 +0200

    make fizzbuzz working

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,9 +1,20 @@
-def fizzbuzz(number):
-    return
+def fizzbuzz(n):
+    if n % 15 == 0:
+        n = 'FizzBuzz'
+    elif n % 3 == 0:
+        n = 'Fizz'
+    elif n % 5 == 0:
+        n = 'Buzz'
+    return n
 
 
 def main():
-    return
+    for i in range (100):
+        number = (i + 1)
+        number = fizzbuzz(number)
+        print (number)
+    return number
+
 
 if __name__ == '__main__':
     main()
