f3f9431e2f371fccae68b6ac97ec0d9707479397
Miklos Csepella <csepella.miki@gmail.com>
2016-06-08 18:46:48 +0200

    added the final version of palindrome and passgen

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,9 +1,18 @@
 def palindrome(str):
-    return
+    word_without_space = str.replace(" ", "")
+    kutya = word_without_space.lower()
+    if kutya == kutya[::-1]:
+        return True
+    else:
+        return False
 
 
 def main():
-    return
+    word = input("Give me a word plz!")
+    if palindrome(word):
+        print("AAAAAhhhh yeah BABY!")
+    else:
+        print("LOSER")
 
 
 if __name__ == '__main__':
--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,9 +1,15 @@
+import random, string
 def passwordgen():
-    return
-
+    password = ""
+    key_list = string.ascii_letters + string.digits + string.punctuation
+    pass_lenght = 8
+    for i in range(pass_lenght):
+        next_index = random.randint(0, len(key_list) - 1)
+        password += key_list[next_index]
+    return password
 
 def main():
-    return
+    print(passwordgen())
 
 
 if __name__ == '__main__':
